7e298044500d01f31d8903c745d4edb966fa5124
ihubabithan <abithan.p.ihub@snsgroups.com>
2025-08-16 21:51:39 +0530

    re deployed

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ app.get('/tweets/:username', async (req, res) => {
     const browser = await puppeteer.launch({
     headless: true,
     executablePath: puppeteer.executablePath(), 
-    args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-http2'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+
     const page = await browser.newPage();
     await page.setUserAgent(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
